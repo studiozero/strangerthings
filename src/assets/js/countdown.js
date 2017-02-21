04f8194da94be49c4564bfea6234e92c96dd1c0e
Shaun Dunne <shaun@studiozero.co>
2017-02-21 18:39:13 +0000

    update to metadata

--- src/assets/js/countdown.js
+++ src/assets/js/countdown.js
@@ -5,31 +5,45 @@
 var target_date = new Date("Oct 31, 2017").getTime();
  
 // variables for time units
-var days, hours, minutes, seconds;
+var days, hours, minutes, seconds, countdown;
+
+var increment = function(){
+
+		var current_date = new Date().getTime();
+		var seconds_left = (target_date - current_date) / 1000;
+ 
+		// do some time calculations
+		days = parseInt(seconds_left / 86400);
+		seconds_left = seconds_left % 86400;
+		 
+		hours = parseInt(seconds_left / 3600);
+		seconds_left = seconds_left % 3600;
+		 
+		minutes = parseInt(seconds_left / 60);
+		seconds = parseInt(seconds_left % 60);
+		 
+		// format countdown string + set tag value
+		countdown = days + " : " + hours + " : " + minutes + " : " + seconds;
+}
+
+var timer = setInterval(increment, 1000)
+
+var prev = null;
 
 AFRAME.registerComponent('countdown', {
+
+	init: function(){
+		prev = countdown;
+	},
 	
-  tick: function (oldData) {
-    var data = this.data;
-    var el = this.el;
+	tick: function () {
 
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
-     
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-     
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-     
-    // format countdown string + set tag value
-    countdown = days + " : " + hours + " : " + minutes + " : " + seconds;
+		if(prev != countdown){
+			prev = countdown;			
+			this.el.setAttribute('text', 'text:'+countdown+';font:#thingsFont;');
+		}
 
-    el.setAttribute('text', 'text:'+countdown+';font:#thingsFont;');
+		// document.body.setAttribute('data-days', days);
 
-  }
+	}
 })
